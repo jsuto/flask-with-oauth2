32da48a6cdb155c3e94d789a5868bcaffe832e2a
Janos SUTO <sj@acts.hu>
2023-03-10 13:00:37 +0100

    Fixed pylint findings for create_system_token.py
    Signed-off-by: Janos SUTO <sj@acts.hu>

--- misc/create_system_token.py
+++ misc/create_system_token.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+"""
+Create a jwt token
+"""
 
-from authlib.jose import JsonWebSignature
 import json
 import os
 import uuid
+from authlib.jose import JsonWebSignature
 
 header = {'alg': 'HS256'}
 
-system_token = str(uuid.uuid4())
-payload = json.dumps({'system_token': system_token, 'customerid': 'fictive'})
+SYSTEM_TOKEN = str(uuid.uuid4())
+payload = json.dumps({'SYSTEM_TOKEN': SYSTEM_TOKEN, 'customerid': 'fictive'})
 secret = os.getenv("CLIENT_SECRET", "aaaa")
 
 jws = JsonWebSignature()
 jwt_token = jws.serialize_compact(header, payload, secret)
 
-print(f"system_token: {system_token}\nJWT token: {jwt_token.decode('utf-8')}")
+print(f"SYSTEM_TOKEN: {SYSTEM_TOKEN}\nJWT token: {jwt_token.decode('utf-8')}")
