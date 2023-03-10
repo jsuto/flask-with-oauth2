a94627b206f88d0aa0c797b6d16299cef6e8aee2
Janos SUTO <sj@acts.hu>
2023-03-10 13:19:45 +0100

    Fixed pylint findings for test_backend_with_system_token.py
    Signed-off-by: Janos SUTO <sj@acts.hu>

--- misc/test_backend_with_system_token.py
+++ misc/test_backend_with_system_token.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
+"""
+Test if the flask app can be accessed with jwt token
+"""
 
 import argparse
-import os
 import requests
 
+
 def main():
+    """
+    Send request to the flask app
+    """
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("-j", "--jwt-token", type=str, help="The jwt token", required=True)
-    parser.add_argument("-u", "--uri", type=str, default="http://localhost:5000", help="The backend uri", required=True)
+    parser.add_argument("-j", "--jwt-token", type=str, help="The jwt token",
+                        required=True)
+    parser.add_argument("-u", "--uri", type=str, default="http://localhost:5000",
+                        help="The backend uri", required=True)
     args = parser.parse_args()
 
     headers = {
-        "Authorization": "Bearer %s" % args.jwt_token
+        "Authorization": f"Bearer {args.jwt_token}"
     }
 
-    response = requests.request("GET", args.uri, headers=headers)
+    response = requests.request("GET", args.uri, headers=headers, timeout=10)
 
     print(response.text)
+
 
 if __name__ == "__main__":
     main()
